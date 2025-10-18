97e91343ba45feb1aaec1ed8814201405859a5b9
Cj <cjx20021013@163.com>
2025-10-18 16:47:11 +0800

    1

--- test/testCall.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __call__(self,name):
-        print("hi"+name)
-    def hello(self,name):
-        print("hello"+name)
-person = Person()
-person("zhangsan")
-person.hello("zhangsan")
